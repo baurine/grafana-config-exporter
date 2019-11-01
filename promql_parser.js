006225de339f42cb8f7ec8c90080ec23c24080cc
baurine <2008.hbl@gmail.com>
2019-11-01 18:09:53 +0800

    implement inject extra tags

---
+++ promql_parser.js
@@ -0,0 +1,137 @@
+const PROMQL_SYMBOLS = '()[]{}'
+const MATH_SIGNS = '+-*/'
+
+function combineExprArr(exprArr) {
+  return exprArr
+    .map(item => (item.type === 'sign' ? ` ${item.val} ` : item.val))
+    .join('')
+}
+
+function isNumber(str) {
+  return !isNaN(parseFloat(str))
+}
+
+class PromQLParser {
+  constructor(promQL) {
+    this.promQL = promQL
+    this.exprArr = [] // item: {val: 'sum', type: 'sign|symbol|fn|metric|tags|duration|metric_tags|const_number', pos: 2}
+    this.curCommonChars = []
+  }
+
+  enqueExpr(exprType, curPos) {
+    this.exprArr.push({
+      val: this.curCommonChars.join(''),
+      type: exprType,
+      pos: curPos - this.curCommonChars.length
+    })
+    this.curCommonChars = []
+  }
+
+  // load1 + load5
+  // (sum(load1) + 5)
+  // 5 + sum(load1)
+  // '5' -> const_num
+  // 'load1' -> metric
+  judgeType() {}
+
+  parse() {
+    let i = 0
+    for (i = 0; i < this.promQL.length; i++) {
+      const char = this.promQL[i]
+
+      if (PROMQL_SYMBOLS.includes(char)) {
+        // symbol
+        switch (char) {
+          case '(':
+            // '(' 之前，如果 tempChars.length > 0，则为函数
+            if (this.curCommonChars.length > 0) {
+              this.enqueExpr('fn', i)
+            }
+            break
+          case ')':
+            // ')' 之前，如果 tempChars.length > 0，则为 metric 或 const_number
+            // 如果 tempChars 为数字，则为 const_number
+            if (this.curCommonChars.length > 0) {
+              if (isNumber(this.curCommonChars.join(''))) {
+                this.enqueExpr('const_number', i)
+              } else {
+                this.enqueExpr('metric', i)
+              }
+            }
+            break
+          case '{':
+            // '{' 之前是 metric，this.tempChars 必须大于 0
+            this.enqueExpr('metric', i)
+            break
+          case '}':
+            // '}' 之前是 tags
+            this.enqueExpr('tags', i)
+            break
+          case '[':
+            // '[' 之前，如果 tempChars 大于 0，则为 metric
+            if (this.curCommonChars.length > 0) {
+              this.enqueExpr('metric', i)
+            }
+            break
+          case ']':
+            // ']' 之前是 duration, this.tempChars 必须大于 0
+            this.enqueExpr('duration', i)
+            break
+        }
+        // 放入 exprArr 中
+        this.exprArr.push({ val: char, type: 'symbol', pos: i })
+      } else if (MATH_SIGNS.includes(char)) {
+        // 如果 curCommonChars 不为空，那它有可能是 metric 或 const_number
+        if (this.curCommonChars.length > 0) {
+          if (isNumber(this.curCommonChars.join(''))) {
+            this.enqueExpr('const_number', i)
+          } else {
+            this.enqueExpr('metric', i)
+          }
+        }
+        this.exprArr.push({ val: char, type: 'sign', pos: i })
+      } else if (char !== ' ') {
+        // common char except ' '
+        this.curCommonChars.push(char)
+      }
+    }
+    if (this.curCommonChars.length > 0) {
+      if (isNumber(this.curCommonChars.join(''))) {
+        this.enqueExpr('const_number', i)
+      } else {
+        this.enqueExpr('metric', i)
+      }
+    }
+  }
+
+  combine() {
+    return combineExprArr(this.exprArr)
+  }
+
+  injectToTags(extraTag) {
+    let newExprArr = [...this.exprArr]
+    for (let i = 0; i < newExprArr.length; i++) {
+      let curExpr = newExprArr[i]
+      if (curExpr.type !== 'metric') {
+        continue
+      }
+
+      // metric
+      let tagsExpr = newExprArr[i + 2]
+      if (tagsExpr && tagsExpr.type === 'tags') {
+        // 有对应的 tags expr
+        tagsExpr.val = tagsExpr.val
+          .split(',')
+          .concat(extraTag)
+          .join(', ')
+      } else {
+        // 没有 tags expr
+        curExpr.val = curExpr.val + '{' + extraTag + '}'
+        curExpr.type = 'metric_tags'
+      }
+    }
+    return combineExprArr(newExprArr)
+  }
+}
+
+module.exports = PromQLParser
--- test/test-promql-parser.js
+++ test/test-promql-parser.js
@@ -1,16 +1,17 @@
-// const PromQLParser = require('./promql_parser.mjs')
-import PromQLParser from './promql_parser.mjs'
+const PromQLParser = require('../promql_parser')
 
 function test() {
   let promQL =
     '(sum(rate(tikv_storage_engine_async_request_duration_seconds_sum{type="write"}[5m]))+5)/sum(rate(tikv_storage_engine_async_request_duration_seconds_count{type="write"}[5m]))'
   let parser = new PromQLParser(promQL)
   parser.parse()
-  console.log(parser.combine())
   console.log(parser.exprArr)
   if (promQL !== parser.combine()) {
     console.error('error')
   }
+  console.log(promQL)
+  console.log(parser.combine())
+  console.log(parser.injectToTags('PLACE_HOLDER'))
 }
 
 test()
