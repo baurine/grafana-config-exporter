fe78c8ad463d276473999f1e7590320456f32dbd
baurine <2008.hbl@gmail.com>
2019-11-25 11:42:29 +0800

    feat - update parse logic

--- index.js
+++ index.js
@@ -1,135 +1,140 @@
-const fetch = require('node-fetch')
-const fs = require('fs')
+const fetch = require("node-fetch");
+const fs = require("fs");
 
-const config = require('./config.json')
-const BASE_URL = config.grafana_server
-const GRAFANA_API_KEY = config.grafana_api_key
-const TARGET_DB_UIDS = config.target_db_uids
+const config = require("./config.json");
+const BASE_URL = config.grafana_server;
+const GRAFANA_API_KEY = config.grafana_api_key;
+const TARGET_DB_UIDS = config.target_db_uids;
 
-const { genNewPromQL } = require('./promql_parser')
+const { genNewPromQL } = require("./promql_parser");
 
 function parseResponse(res) {
   if (res.status === 204) {
-    return {}
+    return {};
   } else if (res.status >= 200 && res.status < 300) {
-    return res.json()
+    return res.json();
   } else {
     return res.json().then(resData => {
-      const errMsg = resData.message || res.statusText
-      const error = new Error(errMsg)
-      throw error
-    })
+      const errMsg = resData.message || res.statusText;
+      const error = new Error(errMsg);
+      throw error;
+    });
   }
 }
 
 async function request(apiPath) {
   return fetch(`${BASE_URL}/api/${apiPath}`, {
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
+      Accept: "application/json",
+      "Content-Type": "application/json",
       Authorization: `Bearer ${GRAFANA_API_KEY}`
     }
   })
     .then(parseResponse)
     .then(data => ({ data }))
-    .catch(err => ({ err }))
+    .catch(err => ({ err }));
 }
 
 async function searchDashboards() {
-  const url = 'search?folderIds=0&type=dash-db'
-  const dbs = await request(url)
-  return dbs.map(db => db.uid)
+  const url = "search?folderIds=0&type=dash-db";
+  const dbs = await request(url);
+  return dbs.map(db => db.uid);
 }
 
 async function fetchDbDetail(dbUid) {
-  const url = `dashboards/uid/${dbUid}`
-  const detail = await request(url)
+  const url = `dashboards/uid/${dbUid}`;
+  const detail = await request(url);
   if (detail.err) {
-    return undefined
+    return undefined;
   }
 
-  const dbDetail = detail.data.dashboard
-  let unitFormats = []
-  const simplifiedDetail = {
-    units: [],
+  const dbDetail = detail.data.dashboard;
 
-    uid: dbDetail.uid,
+  const simplifiedDetail = {
+    sectionKey: dbDetail.uid,
     title: dbDetail.title,
 
     panels: (dbDetail.panels || []).map(panel => {
-      if (panel.yaxes) {
-        unitFormats.push(panel.yaxes[0].format)
-      }
-      return {
-        id: panel.id,
+      let newPanel = {
+        panelKey: panel.id,
         title: panel.title,
 
-        targets: transformTargets(panel.targets),
-        yaxes: panel.yaxes,
-
         subPanels: (panel.panels || []).map(subPanel => {
-          if (subPanel.yaxes) {
-            unitFormats.push(subPanel.yaxes[0].format)
-          }
           return {
-            id: subPanel.id,
+            subPanelKey: subPanel.id,
             title: subPanel.title,
-            description: subPanel.description,
 
             targets: transformTargets(subPanel.targets),
-            yaxes: subPanel.yaxes
-          }
+            yaxis: (subPanel.yaxes || []).map(yaxis => ({
+              format: yaxis.format,
+              logBase: yaxis.logBase
+            }))[0]
+          };
         })
+      };
+      if (panel.targets) {
+        newPanel.subPanels.push({
+          title: panel.title,
+          targets: transformTargets(panel.targets),
+          yaxis: (panel.yaxes || []).map(yaxis => ({
+            format: yaxis.format,
+            logBase: yaxis.logBase
+          }))[0]
+        });
       }
+      return newPanel;
     })
-  }
-  simplifiedDetail.units = [...new Set(unitFormats)]
-  return simplifiedDetail
+  };
+  return simplifiedDetail;
 }
 
 function parseArgv() {
-  let argus = {}
+  let argus = {};
   process.argv.slice(2).map(element => {
-    const matches = element.match('--([a-zA-Z0-9_]+)=(.*)')
+    const matches = element.match("--([a-zA-Z0-9_]+)=(.*)");
     if (matches) {
-      const key = matches[1]
-      let value = matches[2].replace(/^['"]/, '').replace(/['"]$/, '')
-      if (key === 'db_uids') {
-        value = value.split(',')
+      const key = matches[1];
+      let value = matches[2].replace(/^['"]/, "").replace(/['"]$/, "");
+      if (key === "db_uids") {
+        value = value.split(",");
       }
-      argus[key] = value
+      argus[key] = value;
     }
-  })
-  return argus
+  });
+  return argus;
 }
 
 function transformTargets(targets) {
   if (targets === undefined) {
-    return undefined
+    return undefined;
   }
   return targets.map(target => ({
-    newExpr: genNewPromQL(target.expr, 'PLACE_HOLDER'),
-    ...target
-  }))
+    // expr: genNewPromQL(target.expr, 'inspectionid="{{inspectionId}}"'),
+    expr: genNewPromQL(target.expr, ""),
+    // oriExpr: target.expr,
+    // newExpr: genNewPromQL(target.expr, 'inspectionid="{{inspectionId}}"'),
+    // format: target.format,
+    legendFormat: target.legendFormat
+  }));
 }
 
 async function main() {
-  const argv = parseArgv()
-  console.log('argv:', argv)
-  const dbUids = argv['db_uids'] || TARGET_DB_UIDS || []
-  console.log('db_uids:', dbUids)
+  const argv = parseArgv();
+  console.log("argv:", argv);
+  const dbUids = argv["db_uids"] || TARGET_DB_UIDS || [];
+  console.log("db_uids:", dbUids);
 
   for (let i = 0; i < dbUids.length; i++) {
-    const dbDetail = await fetchDbDetail(dbUids[i])
-    const jsonContent = JSON.stringify(dbDetail, null, 2)
-    const filePath = `./output/${dbDetail.title.toLowerCase()}.json`
+    const dbDetail = await fetchDbDetail(dbUids[i]);
+    const jsonContent = JSON.stringify(dbDetail, null, 2);
+    const filePath = `./output/${dbDetail.title.toLowerCase()}.json`;
     fs.writeFile(filePath, jsonContent, err => {
       if (err) {
-        console.log(err)
+        console.log(err);
       }
-      console.log(`write result to ${filePath}`)
-    })
+      console.log(`write result to ${filePath}`);
+    });
   }
 }
 
-main()
+main();
--- promql_parser.js
+++ promql_parser.js
@@ -1,10 +1,12 @@
-const PROMQL_SYMBOLS = '()[]{}'
-const MATH_SIGNS = '+-*/'
+const PROMQL_SYMBOLS = "()[]{}";
+const MATH_SIGNS = "+-*/";
 
 function combineExprArr(exprArr) {
   return exprArr
-    .map(item => (item.type === 'sign' ? ` ${item.val} ` : item.val))
-    .join('')
+    .map(item =>
+      item.type === "sign" || item.type === "by" ? ` ${item.val} ` : item.val
+    )
+    .join("");
 }
 
 // load1 + load5
@@ -13,133 +15,178 @@ function combineExprArr(exprArr) {
 // '5' -> const_num
 // 'load1' -> metric
 function isNumber(str) {
-  return !isNaN(parseFloat(str))
+  return !isNaN(parseFloat(str));
 }
 
 class PromQLParser {
   constructor(promQL) {
-    this.promQL = promQL
-    this.exprArr = [] // each item: {val: 'sum', type: 'sign|symbol|fn|metric|tags|duration|metric_tags|const_number', pos: 2}
-    this.curCommonChars = []
+    this.promQL = promQL;
+    this.exprArr = []; // each item: {val: 'sum', type: 'sign|symbol|fn|metric|tags|duration|metric_tags|const_number|by|remain', pos: 2}
+    this.curCommonChars = [];
 
-    this.parse()
+    this.parse();
   }
 
   enqueExpr(exprType, curPos) {
     this.exprArr.push({
-      val: this.curCommonChars.join(''),
+      val: this.curCommonChars.join(""),
       type: exprType,
       pos: curPos - this.curCommonChars.length
-    })
-    this.curCommonChars = []
+    });
+    this.curCommonChars = [];
   }
 
   enqueueMetricOrNumber(pos) {
-    if (isNumber(this.curCommonChars.join(''))) {
-      this.enqueExpr('const_number', pos)
+    if (isNumber(this.curCommonChars.join(""))) {
+      this.enqueExpr("const_number", pos);
     } else {
-      this.enqueExpr('metric', pos)
+      this.enqueExpr("metric", pos);
     }
   }
 
   parse() {
-    let i = 0
+    let i = 0;
+    let reachEnd = false; // 以 by 为标准
+    let enterTags = false;
     for (i = 0; i < this.promQL.length; i++) {
-      const char = this.promQL[i]
+      const char = this.promQL[i];
+
+      if (enterTags && char !== "}") {
+        this.curCommonChars.push(char);
+        continue;
+      }
 
       if (PROMQL_SYMBOLS.includes(char)) {
         // symbol
         switch (char) {
-          case '(':
-            // '(' 之前，如果 tempChars.length > 0，则为函数
+          case "(":
+            // '(' 之前，如果 tempChars.length > 0，则为函数 或 by
             if (this.curCommonChars.length > 0) {
-              this.enqueExpr('fn', i)
+              if (this.curCommonChars.join("") === "by") {
+                // parse 结束
+                reachEnd = true;
+                this.enqueExpr("by", i);
+                // enque 剩下所有
+                this.exprArr.push({
+                  val: this.promQL.slice(i),
+                  type: "remain",
+                  pos: i
+                });
+              } else {
+                this.enqueExpr("fn", i);
+              }
             }
-            break
-          case ')':
+            break;
+          case ")":
             // ')' 之前，如果 tempChars.length > 0，则为 metric 或 const_number
             // 如果 tempChars 为数字，则为 const_number
             if (this.curCommonChars.length > 0) {
-              this.enqueueMetricOrNumber(i)
+              this.enqueueMetricOrNumber(i);
             }
-            break
-          case '{':
+            break;
+          case "{":
             // '{' 之前是 metric，this.tempChars 必须大于 0
-            this.enqueExpr('metric', i)
-            break
-          case '}':
+            this.enqueExpr("metric", i);
+            enterTags = true;
+            break;
+          case "}":
             // '}' 之前是 tags
-            this.enqueExpr('tags', i)
-            break
-          case '[':
+            this.enqueExpr("tags", i);
+            enterTags = false;
+            break;
+          case "[":
             // '[' 之前，如果 tempChars 大于 0，则为 metric
             if (this.curCommonChars.length > 0) {
-              this.enqueExpr('metric', i)
+              this.enqueExpr("metric", i);
             }
-            break
-          case ']':
+            break;
+          case "]":
             // ']' 之前是 duration, this.tempChars 必须大于 0
-            this.enqueExpr('duration', i)
-            break
+            this.enqueExpr("duration", i);
+            break;
         }
-        // 放入 exprArr 中
-        this.exprArr.push({ val: char, type: 'symbol', pos: i })
+        if (!reachEnd) {
+          // 放入 exprArr 中
+          this.exprArr.push({ val: char, type: "symbol", pos: i });
+        }
       } else if (MATH_SIGNS.includes(char)) {
         // 如果 curCommonChars 不为空，那它有可能是 metric 或 const_number
+        // edge case: "sum(rate(tikv_thread_cpu_seconds_total{name=~\"apply_[0-9]+\"}[1m])) by (instance)"
+        // edge case: "go_memstats_heap_inuse_bytes{job=~\"tidb.*\"}"
+        // 如果 * 前面是 . 号，那么这个 * 号并不是数学符号
         if (this.curCommonChars.length > 0) {
-          this.enqueueMetricOrNumber(i)
+          // TODO: remove
+          if (this.curCommonChars[this.curCommonChars.length - 1] === ".") {
+            // 说明是通配符
+            this.curCommonChars.push(char);
+          } else {
+            this.enqueueMetricOrNumber(i);
+            this.exprArr.push({ val: char, type: "sign", pos: i });
+          }
+        } else {
+          this.exprArr.push({ val: char, type: "sign", pos: i });
         }
-        this.exprArr.push({ val: char, type: 'sign', pos: i })
-      } else if (char !== ' ') {
+      } else if (char !== " ") {
         // common char except ' '
-        this.curCommonChars.push(char)
+        this.curCommonChars.push(char);
+        if (char === ",") {
+          // 在 ',' 后面补一个空格
+          this.curCommonChars.push(" ");
+        }
+      }
+      if (reachEnd) {
+        break;
       }
     }
     if (this.curCommonChars.length > 0) {
-      this.enqueueMetricOrNumber(i)
+      this.enqueueMetricOrNumber(i);
     }
   }
 
   combine() {
-    return combineExprArr(this.exprArr)
+    return combineExprArr(this.exprArr);
   }
 
   injectToTags(extraTag, cleanVariablesInTags = false) {
-    let newExprArr = [...this.exprArr]
+    let newExprArr = [...this.exprArr];
     for (let i = 0; i < newExprArr.length; i++) {
-      let curExpr = newExprArr[i]
-      if (curExpr.type !== 'metric') {
-        continue
+      let curExpr = newExprArr[i];
+      if (curExpr.type !== "metric") {
+        continue;
       }
 
       // metric
-      let tagsExpr = newExprArr[i + 2]
-      if (tagsExpr && tagsExpr.type === 'tags') {
+      let tagsExpr = newExprArr[i + 2];
+      if (tagsExpr && tagsExpr.type === "tags") {
         // 有对应的 tags expr
         tagsExpr.val = tagsExpr.val
-          .split(',')
+          .split(",")
+          .map(tag => tag.trim())
           .filter(tag =>
             // 清除含变量的 tag，比如 instance=~"$instance"
             cleanVariablesInTags ? tag.indexOf('"$') === -1 : true
-          )
-          .concat(extraTag)
-          .join(', ')
+          );
+        if (extraTag !== "") {
+          tagsExpr.val = tagsExpr.val.concat(extraTag);
+        }
+        tagsExpr.val = tagsExpr.val.join(", ");
       } else {
         // 没有 tags expr
-        curExpr.val = curExpr.val + '{' + extraTag + '}'
-        curExpr.type = 'metric_tags'
+        curExpr.val = curExpr.val + "{" + extraTag + "}";
+        curExpr.type = "metric_tags";
       }
     }
-    return combineExprArr(newExprArr)
+    return combineExprArr(newExprArr);
   }
 }
 
 ////////////////////////////////////////////
 
 function genNewPromQL(oriPromQL, placeholder) {
-  return new PromQLParser(oriPromQL).injectToTags(placeholder, true)
+  const parser = new PromQLParser(oriPromQL);
+  return parser.injectToTags(placeholder, true);
 }
 
 ////////////////////////////////////////////
 
-module.exports = { PromQLParser, genNewPromQL }
+module.exports = { PromQLParser, genNewPromQL };
